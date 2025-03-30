765e55dba1477acb6ddb8420bbdd3c2d15094942
williamdsq <williamdsq@gmail.com>
2025-03-30 18:20:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))  # O Render define essa variável
     app.run(host='0.0.0.0', port=port)
